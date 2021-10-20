e6ba7c68bf0c8320697b547e8dd4f3d0cc85e5b6
MarcoNanka <marco.nanka@tum.de>
2021-10-20 17:32:06 +0200

    Updated

--- Marco Nanka/jquery/main.js
+++ Marco Nanka/jquery/main.js
@@ -78,7 +78,7 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e) => {
   setTimeout(()=>{
     splash.classList.add('display-none');
-  }, 3000);
+  }, 2000);
   
 })
    
